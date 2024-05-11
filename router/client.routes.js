00174a9ac8f1c7bf14cf2fed47689a7d8eb537ef
Hoàng Huy <hoangleonard1223@gmail.com>
2024-05-11 08:53:46 +0700

    edited

--- router/client.routes.js
+++ router/client.routes.js
@@ -12,6 +12,12 @@ router.use('/giai-dau', checkLogin, (req, res, next) => {
 router.use('/membership', checkLogin, (req, res, next) => {
     res.render('membership')
 })
+router.use('/membership_payment', checkLogin, (req, res, next) => {
+    res.render('membership_payment')
+})
+router.use('/payment_complete', checkLogin, (req, res, next) => {
+    res.render('membership_payment_complete')
+})
 router.use('/orderTable', checkLogin, (req, res, next) => {
     res.render('orderTable')
 })
